7e26e620e5ee65c40d122a64d0711274af5effc5
yonibar1 <yonibar1999@gmail.com>
2021-02-17 13:50:58 +0200

    first commit

---
+++ js/map-controller.js
@@ -0,0 +1,83 @@
+import { mapService } from './services/map-service.js'
+
+var gMap;
+console.log('Main!');
+
+mapService.getLocs()
+    .then(locs => console.log('locs', locs))
+
+window.onload = () => {
+
+    document.querySelector('.btn').addEventListener('click', (ev) => {
+        console.log('Aha!', ev.target);
+        panTo(35.6895, 139.6917);
+    })
+
+    initMap()
+        .then(() => {
+            addMarker({ lat: 32.0749831, lng: 34.9120554 });
+        })
+        .catch(() => console.log('INIT MAP ERROR'));
+
+    getPosition()
+        .then(pos => {
+            console.log('User position is:', pos.coords);
+        })
+        .catch(err => {
+            console.log('err!!!', err);
+        })
+}
+
+function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            console.log('Map!', gMap);
+        })
+}
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!'
+    });
+    return marker;
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng);
+    gMap.panTo(laLatLng);
+}
+
+// This function provides a Promise API to the callback-based-api of getCurrentPosition
+function getPosition() {
+    console.log('Getting Pos');
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+}
+
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = ''; //TODO: Enter your API Key
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
+
+
---
+++ js/services/map-service.js
@@ -0,0 +1,14 @@
+export const mapService = {
+    getLocs
+}
+var locs = [{ lat: 11.22, lng: 22.11 }]
+
+function getLocs() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(locs);
+        }, 2000)
+    });
+}
+
+
